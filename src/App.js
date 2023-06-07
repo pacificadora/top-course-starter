e14129f13a6c767e1cd4aa9c9ffaeb6e8a6b0ba1
dire_wolf <aman.singhal@coditas.com>
2023-06-07 06:57:32 +0530

    adding all the files in the git repo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import data from './data'
+
+const App = () => {
+
+  const [tours, setTours] = useState()
+
+  return (
+  <div>
+    APP
+  </div>);
+};
+
+export default App;
